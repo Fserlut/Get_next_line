2f4be483253f750fb6e09a6eb3b1721d344efe52
Ilya <zakharovvilya@yandex.ru>
2019-06-26 19:52:30 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -12,9 +12,31 @@
 
 #include "get_next_line.h"
 
-char			*get_next_line_cut()
+int				ft_new_line(char **fd_save, char **line, int fd, int read_o)
 {
-    return (NULL);
+	char		*tmp;
+	int			len;
+
+	len = 0;
+	while (fd_save[fd][len] != '\n' && fd_save[fd][len] != '\0')
+		len++;
+	if (fd_save[fd][len] == '\n')
+	{
+		*line = ft_strsub(fd_save[fd], 0, len);
+		tmp = ft_strdup(fd_save[fd] + len + 1);
+		ft_strdel(&fd_save[fd]);
+		fd_save[fd] = tmp;
+		if (fd_save[fd][0] == '\0')
+			ft_strdel(&fd_save[fd]);
+	}
+	else if (fd_save[fd][len] == '\0')
+	{
+		if (read_o == BUFF_SIZE)
+			return (get_next_line(fd, line));
+		*line = ft_strdup(fd_save[fd]);
+		ft_strdel(&fd_save[fd]);
+	}
+	return (1);
 }
 
 int				get_next_line(const int fd, char **line)
@@ -22,20 +44,24 @@ int				get_next_line(const int fd, char **line)
 	static char	*fd_save[1000];
 	char		buff[BUFF_SIZE + 1];
 	char		*tmp;
-	int			read_b;
+	int			read_o;
 
 	if (fd < 0 || line == NULL)
 		return (-1);
-	while ((read_b = read(fd, buff, BUFF_SIZE)) > 0)
+	while ((read_o = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		buff[read_b] = '\0';
+		buff[read_o] = '\0';
 		if (fd_save[fd] == NULL)
 			fd_save[fd] = ft_strnew(1);
 		tmp = ft_strjoin(fd_save[fd], buff);
 		ft_strdel(&fd_save[fd]);
 		fd_save[fd] = ft_strdup(tmp);
 		if (ft_strchr(buff, '\n'))
-			break;
+			break ;
 	}
-	return (0);
+	if (read_o < 0)
+		return (-1);
+	else if (read_o == 0 && (fd_save[fd] == NULL || fd_save[fd][0] == '\0'))
+		return (0);
+	return (ft_new_line(fd_save, line, fd, read_o));
 }
