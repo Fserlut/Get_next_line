3450aa4fc86bcf40b6b2c2f339b5658d82c5107c
Ilya <zakharovvilya@yandex.ru>
2019-07-02 21:45:09 +0300

    protect malloc

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,26 @@
 /*   By: fserlut <fserlut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 11:49:13 by fserlut           #+#    #+#             */
-/*   Updated: 2019/06/22 10:36:35 by fserlut          ###   ########.fr       */
+/*   Updated: 2019/07/02 21:11:43 by fserlut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		ft_new_line(char **fd_save, char **line, int fd)
+int				ft_new_line(char **fd_save, char **line, int fd)
 {
-	char	*tmp;
-	int		len;
+	char		*tmp;
+	int			len;
 
 	len = 0;
 	while (fd_save[fd][len] != '\n' && fd_save[fd][len] != '\0')
 		len++;
 	if (fd_save[fd][len] == '\n')
 	{
-		*line = ft_strsub(fd_save[fd], 0, len);
-		tmp = ft_strdup(fd_save[fd] + len + 1);
+		if ((*line = ft_strsub(fd_save[fd], 0, len)) == NULL)
+			return (-1);
+		if ((tmp = ft_strdup(fd_save[fd] + len + 1)) == NULL)
+			return (-1);
 		ft_strdel(&fd_save[fd]);
 		fd_save[fd] = tmp;
 		if (fd_save[fd][0] == '\0')
@@ -31,13 +33,14 @@ int		ft_new_line(char **fd_save, char **line, int fd)
 	}
 	else if (fd_save[fd][len] == '\0')
 	{
-		*line = ft_strdup(fd_save[fd]);
+		if ((*line = ft_strdup(fd_save[fd])) == NULL)
+			return (-1);
 		ft_strdel(&fd_save[fd]);
 	}
 	return (1);
 }
 
-int		get_next_line(const int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
 	static char	*fd_save[10240];
 	char		buff[BUFF_SIZE + 1];
@@ -50,8 +53,10 @@ int		get_next_line(const int fd, char **line)
 	{
 		buff[read_o] = '\0';
 		if (fd_save[fd] == NULL)
-			fd_save[fd] = ft_strnew(1);
-		tmp = ft_strjoin(fd_save[fd], buff);
+			if ((fd_save[fd] = ft_strnew(1)) == NULL)
+				return (-1);
+		if ((tmp = ft_strjoin(fd_save[fd], buff)) == NULL)
+			return (-1);
 		ft_strdel(&fd_save[fd]);
 		fd_save[fd] = tmp;
 		if (ft_strchr(buff, '\n'))
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: fserlut <fserlut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 19:28:13 by fserlut           #+#    #+#             */
-/*   Updated: 2019/06/20 15:01:37 by fserlut          ###   ########.fr       */
+/*   Updated: 2019/07/02 20:44:58 by fserlut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE	10000000
+# define BUFF_SIZE	1
 
 # include <sys/types.h>
 # include <sys/uio.h>
